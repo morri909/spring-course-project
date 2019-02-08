13744cdbd2ff7fe321219af4466ca775f2f1ee76
Michael Morrison <morri909@umn.edu>
2019-02-07 18:24:06 -0600

    Section 4, Lession 20

--- src/main/java/com/spydrone/mvc/controller/HomeController.java
+++ src/main/java/com/spydrone/mvc/controller/HomeController.java
@@ -1,13 +1,23 @@
 package com.spydrone.mvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.spydrone.mvc.data.entities.Project;
 
 @Controller
 public class HomeController {
 
-	@RequestMapping("/home")
-	public String goHome() {
+	@RequestMapping("/")
+	public String goHome(Model model) {
+		Project project = new Project();
+		project.setName("First Project");
+		project.setSponsor("NASA");
+		project.setDescription("This is a simple project sponsored by NASA");
+
+		model.addAttribute("currentProject", project);
+
 		return "home";
 	}
 }
