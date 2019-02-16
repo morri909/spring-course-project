3d471da0de62aaf873b491116e68aaacf013d176
Michael Morrison <morri909@umn.edu>
2019-02-16 17:06:03 -0600

    Section 7, Lecture 43

--- src/main/java/com/spydrone/mvc/data/entities/Project.java
+++ src/main/java/com/spydrone/mvc/data/entities/Project.java
@@ -3,10 +3,13 @@ package com.spydrone.mvc.data.entities;
 import java.math.BigDecimal;
 import java.util.List;
 
+import org.hibernate.validator.constraints.NotBlank;
+
 public class Project {
 
 	private Long projectId;
 	private String name;
+	@NotBlank
 	private String description;
 	private Sponsor sponsor;
 	private BigDecimal authorizedHours;
