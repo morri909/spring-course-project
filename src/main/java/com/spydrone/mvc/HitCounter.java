ab578aeab18f9bfc995d16a86c25f16d73d817e0
Michael Morrison <morri909@umn.edu>
2019-02-19 18:47:07 -0600

    Section 9, Lecture 56

---
+++ src/main/java/com/spydrone/mvc/HitCounter.java
@@ -0,0 +1,19 @@
+package com.spydrone.mvc;
+
+public class HitCounter {
+
+	private int hits;
+
+	public HitCounter() {
+		System.out.println("Hit Cunter Instantieated");
+	}
+
+	public int getHits() {
+		return hits;
+	}
+
+	public void setHits(int hits) {
+		this.hits = hits;
+	}
+
+}
